2c7378f99625357871a2a80e7ed1cb1cd7a4069a
Sanket Khardekar <sanket.khardekar@mindbowser.com>
2022-06-21 22:26:32 +0530

    Added Redux and Tabs Panel

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Badge, Box, Tab, Tabs } from "@mui/material";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import "./App.css";
+import TabPanel from "./components/TabPanel";
+import CharacterScreen from "./screens/CharacterScreen";
+import FavoritesScreen from "./screens/FavroitesScreen";
 function App() {
+  const [tabValue, setTabValue] = useState(0);
+  const favLength = useSelector((state) => state.favroite.favorites).length;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Box sx={{ width: "100%", borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          textColor="inherit"
+          indicatorColor="secondary"
+          value={tabValue}
+          onChange={(e, val) => {
+            setTabValue(val);
+          }}
+          centered
         >
-          Learn React
-        </a>
+          <Tab sx={{ color: "white" }} label="Characters" />
+            <Tab sx={{ color: "white" }} label="Favorites" />
+        </Tabs>
+      </Box>
+      <header className="App-header">
+        <TabPanel value={tabValue} index={0}>
+          <CharacterScreen />
+        </TabPanel>
+        <TabPanel value={tabValue} index={1}>
+          <FavoritesScreen />
+        </TabPanel>
       </header>
     </div>
   );
