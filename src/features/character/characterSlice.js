dab0a8e73fc77a1f8bc91abcce8ede8651f41be1
Sanket Khardekar <sanket.khardekar@mindbowser.com>
2022-06-21 22:28:41 +0530

    Created Redux Slices with async

---
+++ src/features/character/characterSlice.js
@@ -0,0 +1,43 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+const defaultUrl="https://rickandmortyapi.com/api/character";
+const initialState={
+    isLoading:false,
+    characters:[],
+    isError:false,
+}
+
+export const getCharacters= createAsyncThunk("character/getCharacters",async()=>{
+    try {
+         const response= await fetch(defaultUrl);
+         if(!response.ok)
+         {
+             throw Error("Something Went Wrong")
+         }
+         const resData= await response.json();
+         return resData.results;    
+    } catch (error) {
+        return error.message
+    }
+})
+
+const characterSlice=createSlice({
+    name:"character",
+    initialState,
+    extraReducers:(builder)=>{
+        builder.addCase(getCharacters.pending,(state)=>{
+            state.isLoading=true;
+        })
+        builder.addCase(getCharacters.fulfilled,(state,action)=>{
+            state.isLoading=false;
+            state.characters=action.payload;
+            state.isError=false;
+        })
+        builder.addCase(getCharacters.rejected,(state,action)=>{
+            state.isLoading=false;
+            state.characters=[];
+            state.isError=true;
+        })
+    }
+});
+
+export default  characterSlice.reducer;
---
+++ src/features/favoriteCharacter/favoriteCharacterSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState={
+    favorites:[],
+}
+
+const favoriteCharacterSlice=createSlice({
+    name:"favoriteCharacter",
+    initialState,
+    reducers:{
+        addFav: (state,action)=>{
+            state.favorites.unshift(action.payload)
+        },
+        removeFav:(state,action)=>{
+            console.log(action.payload);
+            state.favorites=state.favorites.filter(item => item.id !== action.payload)
+        }
+    }
+});
+
+export const {addFav,removeFav}=favoriteCharacterSlice.actions;
+export default favoriteCharacterSlice.reducer
