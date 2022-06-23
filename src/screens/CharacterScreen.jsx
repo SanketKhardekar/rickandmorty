c8b9bb0560c320254c3d8a010b748e910966899f
Sanket Khardekar <sanket.khardekar@mindbowser.com>
2022-06-23 18:08:50 +0530

    Added Pagination Component

--- src/screens/CharacterScreen.jsx
+++ src/screens/CharacterScreen.jsx
@@ -1,19 +1,36 @@
-import { Fragment, useEffect } from "react";
-import { Grid, CircularProgress } from "@mui/material";
+import { Fragment, useEffect, useState } from "react";
+import { Grid, CircularProgress, Pagination } from "@mui/material";
 import CharacterList from "../components/CharacterList";
 import { useSelector, useDispatch } from "react-redux";
 import { getCharacters } from "../features/character/characterSlice";
 const CharacterScreen = (props) => {
+  const [page,setPage]=useState(0);
   const dispatch = useDispatch();
-  const { filterText } =props;
+  const { filterText } = props;
   useEffect(() => {
-    if (filterText.trim().length > 0) {
-      dispatch(getCharacters(filterText));
+    let text=filterText;
+    let pageNumber=page;
+    if(page > 0)
+    {
+      if(text.trim().length <= 0)
+      {
+        text+=`?page=${pageNumber}`;
+      }
+      else 
+      {
+        text+=`&page=${pageNumber}`;
+        setPage(0);
+      }
+    }
+    if (text.trim().length > 0) {
+      dispatch(getCharacters(text));
     } else {
       dispatch(getCharacters());
     }
-  }, [dispatch, filterText]);
-  const { characters, isLoading} = useSelector((state) => state.character);
+  }, [dispatch, filterText,page]);
+  const { characters, isLoading, totalPages } = useSelector(
+    (state) => state.character
+  );
   return (
     <Fragment>
       {isLoading ? (
@@ -23,6 +40,29 @@ const CharacterScreen = (props) => {
       ) : (
         <Fragment>
           <CharacterList characters={characters} />
+          {totalPages > 0 ? (
+            <div
+              style={{
+                marginTop:"20px",
+                height:"100px",
+                backgroundColor:"#D6D5A8",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Pagination
+                color="primary"
+                page={page}
+                count={totalPages}
+                onChange={(e,index)=>{ setPage(index)}}
+                size="large"
+                sx={{
+                  color:"white"
+                }}
+              />
+            </div>
+          ) : null}
         </Fragment>
       )}
     </Fragment>
