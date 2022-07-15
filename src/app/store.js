6244b15ac87cc66bc23aa57a0ed5fb7b7d00d819
Sanket Khardekar <sanket.khardekar@mindbowser.com>
2022-07-15 16:46:13 +0530

    Added Auth Reducer

--- src/app/store.js
+++ src/app/store.js
@@ -2,9 +2,11 @@ import { applyMiddleware, configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import characterReducer from "../features/character/characterSlice";
 import favoriteReducer from "../features/favoriteCharacter/favoriteCharacterSlice"
+import authReducer from "../features/auth/authSlice";
 export const store= configureStore({
     reducer:{   
         character: characterReducer,
         favroite: favoriteReducer,
+        auth: authReducer,
     }
 },applyMiddleware(thunk))
